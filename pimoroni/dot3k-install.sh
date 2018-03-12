145b18d97bc67ffbbf917d517dffaf18f3370ce6
Raphael Guntersweiler <raphael@rgunti.ch>
2018-03-12 23:03:04 +0100

    Added cleanup and sudo

--- pimoroni/dot3k-install.sh
+++ pimoroni/dot3k-install.sh
@@ -5,5 +5,9 @@ git clone https://github.com/pimoroni/displayotron.git /tmp/dot3k
 cd /tmp/dot3k/library
 
 # Install (Py2 & Py3)
-python ./setup.py install
-python3 ./setup.py install
+sudo python ./setup.py install
+sudo python3 ./setup.py install
+
+# Cleanup
+cd /tmp
+sudo rm -rf /tmp/dot3k
